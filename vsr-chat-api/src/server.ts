9f82196af11422bf62a63ae496c52b866ff00ebc
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-04-07 16:58:22 +0200

    add post to chat

--- vsr-chat-api/src/server.ts
+++ vsr-chat-api/src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { StreamChat } from "stream-chat";
+import OpenAI from "openai";
+import { GoogleGenAI } from "@google/genai";
 import { v4 as uuid4 } from "uuid";
 
 dotenv.config();
@@ -18,6 +20,16 @@ const chatClient = StreamChat.getInstance(
   process.env.STREAM_API_KEY!,
   process.env.STREAM_API_SECRET!
 );
+
+// Initialise Open AI
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Initialise Gemini API
+const gemini = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
 // Register user with Stream Chat
 app.post("/register", async (req: Request, res: Response): Promise<any> => {
@@ -44,6 +56,53 @@ app.post("/register", async (req: Request, res: Response): Promise<any> => {
 
     res.status(201).json({ userId, name, email });
   } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Send message to Chat AI
+app.post("/chat", async (req: Request, res: Response): Promise<any> => {
+  const { message, userId } = req.body || {};
+
+  if (!message || !userId) {
+    return res.status(400).json({ error: "Message and user are required" });
+  }
+
+  try {
+    const { users } = await chatClient.queryUsers({ id: userId });
+
+    if (!users.length) {
+      return res
+        .status(404)
+        .json({ error: "User not found. Please register first." });
+    }
+
+    // Post message to Open AI GPT-4
+    // const response = await openai.chat.completions.create({
+    //   model: "gpt-4",
+    //   messages: [{ role: "user", content: message }],
+    // });
+
+    // Post message to Gemini API
+    const response = await gemini.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: message,
+    });
+
+    const aiMessage: string = response.text ?? "No response from chat bot";
+
+    // Create or fetch channel
+    const channel = chatClient.channel("messaging", `chat-${userId}`, {
+      name: "AI Chat",
+      created_by_id: "ai_bot",
+    });
+
+    await channel.create();
+    await channel.sendMessage({ text: aiMessage, user_id: "ai_bot" });
+
+    res.status(200).json({ reply: aiMessage });
+  } catch (error) {
+    console.log("Error generating ai response", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
